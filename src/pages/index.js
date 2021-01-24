eda82b5b3570f1e8c738a64d4f67fcb25f8a8f62
Adrià Sala <adriasala124@gmail.com>
2021-01-24 10:39:00 +0100

    Added menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,30 +3,51 @@ import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import BackgroundSection from '../components/Globals/BackgroundSection'
-import Info from '../components/Home/Info'
+import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/Info"
+import Menu from "../components/Home/Menu"
 
-const IndexPage = ({data}) => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <BackgroundSection 
-      img={data.img.childImageSharp.fluid} 
-      title="coffee shop" 
-      styleClass="default-background" />
-      <Info />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="coffee shop"
+      styleClass="default-background"
+    />
+    <Info />
+    <Menu items={data.menu} />
   </Layout>
 )
 
 export const query = graphql`
-{
-  img:file(relativePath:{eq:"default-background.jpeg"}){
-    childImageSharp {
-      fluid {
-        ...GatsbyImageSharpFluid_tracedSVG
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed
+            }
+          }
+        }
       }
     }
   }
-}
 `
 
 export default IndexPage
